3037210a4e7715b45bf280f57f196a3b48d48832
Ying <ying.feifan@avanade.com>
2020-02-26 22:47:32 -0300

    finalização do token

--- Config/config.js
+++ Config/config.js
@@ -6,21 +6,21 @@ const config = () =>{
             return{
                 bd_url:'mongodb+srv://admin:admin@api-hsdbr.mongodb.net/test?retryWrites=true&w=majority',
                 jwt_password:'sky2019',
-                jwt_expires_in: '1d',
+                jwt_expires_in: '1h',
             }
         }
         case 'hml':{
             return{
                 bd_string:'mongodb+srv://admin:admin@api-hsdbr.mongodb.net/test?retryWrites=true&w=majority',
                 jwt_password:'sky2019',
-                jwt_expires_in: '1d',
+                jwt_expires_in: '1h',
             }
         }
         case 'prod':{
             return{
                 bd_string:'mongodb+srv://admin:admin@api-hsdbr.mongodb.net/test?retryWrites=true&w=majority',
                 jwt_password:'sky2019',
-                jwt_expires_in: '1d',
+                jwt_expires_in: '1h',
             }
         }
 
