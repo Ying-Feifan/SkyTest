0e09585959afa13a98e59468c311b37e7dd2efd8
Ying <ying.feifan@avanade.com>
2020-03-02 16:04:29 -0300

    first unit test

--- test/skypeRouter.test.js
+++ test/skypeRouter.test.js
@@ -2,37 +2,54 @@
 const request = require('supertest');
 const router = require('../Router/skyRouter');
 
+
 describe('POST /sign-up', function () {
   it('sign-up success', function (done) {
+    const mock = {
+        nome: 'test2',
+        email: 'teste2@hotmail.com',
+        senha: '123456',
+        telefones:[
+          {
+          numero:'999999999',
+          ddd:'11'
+          }
+        ]
+    };
     request(router)
-      .post('/sign-in')
+      .post('/sign-up')
+      .send(mock)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
     done();
   });
 });
 
 describe('POST /sign-in', function () {
   it('sign-in success', function (done) {
+    const mock = {
+      email: 'teste2@hotmail.com',
+      senha: '123456'
+    };
     request(router)
       .post('/sign-in')
-      .send({
-        email: 'teste2@hotmail.com',
-        senha: '123456'
-      })
+      .send(mock)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, done);
+      .expect(200);
     done();
   });
 });
 
+describe('GET /find-user', function () {
+  it('sign-in success', function (done) {
+    request(router)
+      .get('/find-user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+});
 
-// describe('GET /find-user', function () {
-//   it('find-user success', function (done) {
-//     request(router)
-//       .get('/find-user')
-//       .set('Accept', 'application/json')
-//       .expect('Content-Type', /json/)
-//       .expect(200, done);
-//     done();
-//   });
-// });
